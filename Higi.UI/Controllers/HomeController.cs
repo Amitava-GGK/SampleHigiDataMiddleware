a782138cc9cab48a42f00df44279f4a84886aeef
Amitava Karan <amitava.karan@ggktech.com>
2017-04-14 15:01:55 +0530

    Change HIgi UI

---
+++ Higi.UI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Higi.Middleware.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Higi.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController() { }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Higi.UI/Controllers/UserController.cs
+++ Higi.UI/Controllers/UserController.cs
@@ -1,15 +1,21 @@
 ﻿using Higi.Middleware.Common;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace Higi.UI.Controllers
 {
-    public class UserController : Controller
+    public class UserController : ApiController
     {
         private List<UserHealthData> userHealthDataList;
+        private static HttpClient httpClient = new HttpClient();
 
         public UserController()
         {
@@ -58,17 +64,36 @@ namespace Higi.UI.Controllers
             };
         }
 
-        // GET: User
-        public ActionResult Index()
+        [HttpPost]
+        public async Task<IHttpActionResult> UpdateUserHealthData(User user)
         {
-            return View();
+            if (user == null)
+            {
+                return BadRequest("User can not be null");
+            }
+
+            var middlewareEndpoint = ConfigurationManager.AppSettings["HigiMiddlewareEndpoint"];
+            var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync($"{middlewareEndpoint}/User/UpdateHealthData", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok();
+            }
+            else
+            {
+                return StatusCode(HttpStatusCode.InternalServerError);
+            }
+
         }
 
-        public ActionResult GetUserHealthData(int userId)
+        [HttpGet]
+        public IHttpActionResult GetUserHealthData(int userId)
         {
             var userHealthData = this.userHealthDataList.FirstOrDefault(u => u.UserId == userId);
 
-            return Json(userHealthData, JsonRequestBehavior.AllowGet);
+            return Ok<UserHealthData>(userHealthData);
         }
     }
 }
